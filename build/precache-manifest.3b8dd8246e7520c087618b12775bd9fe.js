92620413de48746a6d3a863e7bd9796b630de9e2
Mmoouu <mauricio_hf@hotmail.com>
2023-03-03 15:10:48 -0300

    Build

---
+++ build/precache-manifest.3b8dd8246e7520c087618b12775bd9fe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "57efc4b53b93ac45a95c",
+    "url": "/static/css/main.7c622e5e.chunk.css"
+  },
+  {
+    "revision": "57efc4b53b93ac45a95c",
+    "url": "/static/js/main.a916b4aa.chunk.js"
+  },
+  {
+    "revision": "73f8abf9ae258dc89f62",
+    "url": "/static/js/runtime~main.3b55aafb.js"
+  },
+  {
+    "revision": "d95014f94373ac341f0d",
+    "url": "/static/css/2.46565656.chunk.css"
+  },
+  {
+    "revision": "d95014f94373ac341f0d",
+    "url": "/static/js/2.4dc56727.chunk.js"
+  },
+  {
+    "revision": "da8b037af6a8c1bd8466",
+    "url": "/static/js/3.557259a5.chunk.js"
+  },
+  {
+    "revision": "32e4c530c090394320e6297e5948c236",
+    "url": "/index.html"
+  }
+];
